97c118711c164589b2cb7b70f87ac1f69c789c36
Zainab <zainabsameer197@gmail.com>
2023-10-23 17:08:52 +0300

    update

--- pages/Showprofile.jsx
+++ pages/Showprofile.jsx
@@ -1,58 +1,62 @@
-
-import { useState, useEffect } from "react"
+import { useState } from 'react'
 import Client from '../services/api'
 
 const UpdateProfile = ({ user }) => {
-  const [value, setValue] = useState({
-    name: '',
-    CPR: '',
-    email: '',
-    phoneNumber: '',
-    pic: null,
-    password: '',
-    confirmPassword: ''
+  // const [formValues, setformValues] = useState({})
+  const [formValues, setFormValues] = useState({
+    name: user.name,
+    CPR: user.CPR , 
+    email: user.email , 
+    phoneNumber: user.phoneNumber ,
+    // pic: null,
   })
-  let { _id } = user
-  useEffect(() => {
-    Client.get(`http://localhost:3001/auth/Showprofile/${_id}`)
-      .then(res => console.log(res.data))
-      .catch(res => console.log(err))
-  }, [])
 
-  
-  
+
+
   const handleChange = (e) => {
-    // setFormValues({ ...formValues, [e.target.name]: e.target.value })
+    const attribute = e.target.name
+    const value = e.target.value
+
+    const currentNewForm = { ...formValues }
+
+    currentNewForm[attribute] = value
+    console.log(currentNewForm)
+    setFormValues(currentNewForm)
   }
-  
+
+  const handleUpdate = async (e) => {
+    e.preventDefault()
+    const response = await Client.put(`/auth/EditProfile/${user._id}`, formValues)
+    console.log(response)
+  }
+
   const handlePicChange = (e) => {
     setFormValues({
       ...formValues,
-      pic: e.target.files[0],
+      pic: e.target.files[0]
     })
     console.log(e.target.files[0])
   }
-  
-  
-  const handleSubmit = async (e) => {
-    e.preventDefault()
-    const formData = new FormData()
-    formData.append('name', value.name)
-    formData.append('CPR', value.CPR)
-    formData.append('email', value.email)
-    formData.append('phoneNumber', value.phoneNumber)
-    formData.append('pic', value.pic)
-    formData.append('password', value.password)
-  }
 
-
-
-
+  // const handleSubmit = async (e) => {
+  //   e.preventDefault()
+  //   const formData = new FormData()
+  //   formData.append('name', value.name)
+  //   formData.append('CPR', value.CPR)
+  //   formData.append('email', value.email)
+  //   formData.append('phoneNumber', value.phoneNumber)
+  //   formData.append('pic', value.pic)
+  //   formData.append('password', value.password)
+  // }
 
   return (
     <div className="signin col">
       <div className="card-overlay centered">
-        <form className="col" onSubmit={handleSubmit} encType="multipart/form-data">
+        <form
+          className="col"
+          onSubmit={handleUpdate}
+          encType="multipart/form-data"
+        >
           <h1> Update</h1>
           <div className="input-wrapper">
             <label htmlFor="name">Full Name</label>
@@ -60,8 +64,9 @@ const UpdateProfile = ({ user }) => {
               onChange={handleChange}
               name="name"
               type="text"
-              placeholder="ُEnter your Full Name"
-              value={value.name}
+              // placeholder="ُEnter your Full Name"
+              // placeholder={formValues.name}
+              value={formValues.name}
               required
             />
           </div>
@@ -73,7 +78,7 @@ const UpdateProfile = ({ user }) => {
               name="CPR"
               type="text"
               placeholder="ُEnter your CPR Number"
-              value={value.CPR}
+              value={formValues.CPR}
               required
             />
           </div>
@@ -85,7 +90,7 @@ const UpdateProfile = ({ user }) => {
               name="email"
               type="email"
               placeholder="example@example.com"
-              value={value.email}
+              value={formValues.email}
               required
             />
           </div>
@@ -96,18 +101,23 @@ const UpdateProfile = ({ user }) => {
               name="phoneNumber"
               type="text"
               placeholder="ُEnter your Phone Number"
-              value={value.phoneNumber}
+              value={formValues.phoneNumber}
               required
             />
           </div>
 
-          <div>
+          {/* <div>
             <label htmlFor="file" className="sr-only">
               Upload your personal photo
             </label>
-            <input id="file" type="file" accept='image/*' onChange={handlePicChange} />
-          </div>
-
+            <input
+              id="file"
+              type="file"
+              accept="image/*"
+              onChange={handlePicChange}
+            />
+          </div> */}
+          {/* 
           <div className="input-wrapper">
             <label htmlFor="password">Password</label>
             <input
@@ -127,8 +137,9 @@ const UpdateProfile = ({ user }) => {
               value={value.confirmPassword}
               required
             />
-          </div>
-          <button onSubmit={handleSubmit}
+          </div> */}
+          <button
+            type='submit'
             // disabled={
             //   !formValues.CPR ||
             //   (!formValues.password &&
