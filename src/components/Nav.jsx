a83f32988bd7cb4e1b84189435d5de7cd197f31f
Zainab <zainabsameer197@gmail.com>
2023-10-23 19:58:20 +0300

    merge

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,62 +5,62 @@ import { NavLink } from 'react-router-dom'
 import '../App.css'
 
 const Nav = () => {
+  const Nav = ({ user, handleLogOut }) => {
+    let userOptions
+    if (user) {
+      userOptions = (
+        <nav>
+          <h3>Welcome {user.email}!</h3>
+          <Link onClick={handleLogOut} to="/">
+            Sign Out
+          </Link>
+        </nav>
+      )
+    }
 
-const Nav = ({ user, handleLogOut }) => {
-  let userOptions
-  if (user) {
-    userOptions = (
+    const publicOptions = (
       <nav>
-        <h3>Welcome {user.email}!</h3>
-        <Link onClick={handleLogOut} to="/">
-          Sign Out
-        </Link>
+        <Link to="/">Home</Link>
+        <Link to="/register">Register</Link>
+        <Link to="/signin">Sign In</Link>
       </nav>
     )
+
+    return (
+      <header>
+        <nav>
+          <NavLink to="/" className={'link-styles'}>
+            ALNOKHATHA
+          </NavLink>
+          <NavLink to="addSlip" className={'link-user '}>
+            Add Slip
+          </NavLink>
+          <NavLink to="slip" className={'link-user '}>
+            All Slips
+          </NavLink>
+          <NavLink to="addBoat" className={'link-user'}>
+            Add Boat
+          </NavLink>
+          <NavLink to="viewBoats" className={'link-user'}>
+            All Boats
+          </NavLink>
+          <NavLink to="Showprofile" className={'link-user '}>
+            Showprofile
+          </NavLink>
+          <NavLink to="/addUsers" className={'link-user '}>
+            Add User
+          </NavLink>
+          <NavLink to="harbors" className={'link-user '}>
+            All Harbors
+          </NavLink>
+          <NavLink to="addHarbor" className={'link-user '}>
+            Add Harbor
+          </NavLink>
+        </nav>
+        {/* {user ? userOptions : publicOptions} */}
+      </header>
+    )
   }
-
-  const publicOptions = (
-    <nav>
-      <Link to="/">Home</Link>
-      <Link to="/register">Register</Link>
-      <Link to="/signin">Sign In</Link>
-    </nav>
-  )
-
-  return (
-    <header>
-            <nav>
-        <NavLink to="/" className={'link-styles'}>
-          ALNOKHATHA
-        </NavLink>
-        <NavLink to="addSlip" className={'link-user '}>
-          Add Slip
-        </NavLink>
-        <NavLink to="slip" className={'link-user '}>
-          All Slips
-        </NavLink>
-        <NavLink to="addBoat" className={'link-user'}>
-          Add Boat
-        </NavLink>
-        <NavLink to="viewBoats" className={'link-user'}>
-          All Boats
-        <NavLink to="Showprofile" className={'link-user '}>
-          Showprofile
-        </NavLink>
-        <NavLink to="/addUsers"  className={'link-user '}>
-         Add User
-        </NavLink>
-        <NavLink to="harbors" className={'link-user '}>
-          All Harbors
-        </NavLink>
-        <NavLink to="addHarbor" className={'link-user '}>
-          Add Harbor
-        </NavLink>
-
-      </nav>
-      {/* {user ? userOptions : publicOptions} */}
-    </header>
-  )
 }
 
 export default Nav
