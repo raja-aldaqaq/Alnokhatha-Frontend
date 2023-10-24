aee883aacf21a581e87030de9f3f1776fe1946ce
Rajaa <raja.aldaqaq@gmail.com>
2023-10-24 19:06:28 +0300

    fix Nav problem

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -66,39 +66,6 @@ const Nav = ({ user, handleLogOut }) => {
         <NavLink to="/" className={'link-styles'}>
           ALNOKHATHA
         </NavLink>
-        <div>
-          <NavLink to="/" className={'link-styles'}>
-            ALNOKHATHA
-          </NavLink>
-          <NavLink to="addSlip" className={'link-user '}>
-            Add Slip
-          </NavLink>
-          <NavLink to="slip" className={'link-user '}>
-            All Slips
-          </NavLink>
-          <NavLink to="addBoat" className={'link-user'}>
-            Add Boat
-          </NavLink>
-          <NavLink to="viewBoats" className={'link-user'}>
-            All Boats
-          </NavLink>
-          <NavLink to="Showprofile" className={'link-user '}>
-            Showprofile
-          </NavLink>
-          <NavLink to="/addUsers" className={'link-user '}></NavLink>
-          {/* <NavLink to="Showprofile" className={'link-user '}>
-          Edit Profile
-        </NavLink> */}
-          {/* <NavLink to="Show" className={'link-user '}> Showprofile</NavLink> */}
-          <NavLink to="/addUsers" className={'link-user '}>
-            Add User
-          </NavLink>
-          <NavLink to="harbors" className={'link-user '}>
-            All Harbors
-          </NavLink>
-          <NavLink to="addHarbor" className={'link-user '}>
-            Add Harbor
-          </NavLink>
         </div>
         {user ? userOptions : publicOptions}
       </nav>
