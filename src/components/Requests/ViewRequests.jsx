effc7a64e15118164231a8b216d6d2db2f5bf0ad
Rajaa <raja.aldaqaq@gmail.com>
2023-10-25 18:29:36 +0300

    view requests

--- src/components/Requests/ViewRequests.jsx
+++ src/components/Requests/ViewRequests.jsx
@@ -3,8 +3,18 @@ const ViewRequests = ({ requests }) => {
     <div>
       {requests.map((request) => (
         <div key={request._id} className="harbor">
-          <h3>Boat : {request.Boat.name}</h3>
-          <h3>User : {request.User.name}</h3>
+          {request.approve? "no": <h5>Approve : "yes"</h5>}
+          <h5>User : {request.User.name}</h5>
+          <h5>Boat : {request.Boat.name}</h5>
+          <h5>Harbor : {request.Harbor.name}</h5>
+          <h5>License Type : {request.Boat.license_type}</h5>
+          {request.approve? null: (<>
+            <h5>Slip Number : {request.boatSlip.number}</h5>
+            <h5>Starting Date : {request.StartDate}</h5>
+            <h5>Expiry Date : {request.ExpiryDate}</h5>
+          </>
+          )}
+
         </div>
       ))}
     </div>
